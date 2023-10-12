c875640442f89068098481ae1520ed013b86f31e
one-loner <one-loner>
2023-10-12 15:21:51 +0300

    Fix L2TP-IPSEC

--- l2tp-ipsec.sh
+++ l2tp-ipsec.sh
@@ -5,6 +5,6 @@ if (($EUID !=0)); then
 fi
 echo installing packages
 apt-get install -y docker docker-compose curl
-docker run --name ipsec-vpn-server --env-file ./vpn.env --restart=always -v ikev2-vpn-data:/etc/ipsec.d -v /lib/modules:/lib/modules:ro -p 500:500/udp -p 4500:4500/udp -d --privileged hwdsl2/ipsec-vpn-server
+docker run --name ipsec-vpn-server --restart=always -v ikev2-vpn-data:/etc/ipsec.d -v /lib/modules:/lib/modules:ro -p 500:500/udp -p 4500:4500/udp -d --privileged hwdsl2/ipsec-vpn-server
 docker logs ipsec-vpn-server
 echo 'Done. You can find login deitals upper.'
